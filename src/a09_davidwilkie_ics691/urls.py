d4ff6aeb8208795507fac397513c450985f1888f
daveywilkie <davey.wilkie@gmail.com>
2013-01-26 21:28:41 -1000

    First commit

---
+++ src/a09_davidwilkie_ics691/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'a09_davidwilkie_ics691.views.home', name='home'),
+    # url(r'^a09_davidwilkie_ics691/', include('a09_davidwilkie_ics691.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)
